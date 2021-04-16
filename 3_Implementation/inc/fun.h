19a35cded56093b0848ea9527e01548e9540f615
Raghavendraam219 <73216258+Raghavendraam219@users.noreply.github.com>
2021-04-16 13:47:07 +0530

    Create fun.h

---
+++ 3_Implementation/inc/fun.h
@@ -0,0 +1,31 @@
+/**
+ * @file fun.h
+ * @author Raghavendra AM (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-12
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include<stdio.h>
+#ifndef __FUN_H__
+#define __FUN_H__
+
+/**
+ * @brief Print all the contact details
+ * 
+ * @return error_t return if it has any error or not
+ */
+void list_all_contact();
+
+void add_new_cont();
+
+void search_cont();
+ 
+void edit_cont();
+
+void del_cont();
+
+#endif
